f9ff24ea0165384e43cf245c6427b0d5b6c17f27
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-03-04 11:57:56 -0800

    basic component layout

---
+++ src/components/KeyContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/KeyContainer.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="container">
+
+			</div>
+		);
+	}
+}
+
+export default App;

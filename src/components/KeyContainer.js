f44d6200db96dc3591e38c7101fc74bd39c10c93
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-03-15 15:12:17 -0700

    rough draft for stacking flat-sharp containers

--- src/components/KeyContainer.js
+++ src/components/KeyContainer.js
@@ -9,16 +9,31 @@ class App extends Component {
 			// keyValue = the value that will be shown in UI, keyName = name used for react data flow purposes
 			<div className="keyContainerWrapper">
 				<Key keyValue="C" keyName="C" isChecked={true} changeKey={this.props.changeKey}/>
-				<Key keyValue="C#/Db" keyName="Db" changeKey={this.props.changeKey}/>
+				<div class="sharpFlatContainer">
+					<Key keyValue="C#/Db" keyName="Db" changeKey={this.props.changeKey}/>
+					<Key keyValue="C#/Db" keyName="Db" changeKey={this.props.changeKey}/>
+				</div>
 				<Key keyValue="D" keyName="D" changeKey={this.props.changeKey}/>
-				<Key keyValue="D#/Eb" keyName="Eb" changeKey={this.props.changeKey}/>
+				<div class="sharpFlatContainer">
+					<Key keyValue="D#/Eb" keyName="Eb" changeKey={this.props.changeKey}/>
+					<Key keyValue="D#/Eb" keyName="Eb" changeKey={this.props.changeKey}/>
+				</div>
 				<Key keyValue="E" keyName="E" changeKey={this.props.changeKey}/>
 				<Key keyValue="F" keyName="F" changeKey={this.props.changeKey}/>
-				<Key keyValue="F#/Gb" keyName="Gb" changeKey={this.props.changeKey}/>
+				<div class="sharpFlatContainer">
+					<Key keyValue="F#/Gb" keyName="Gb" changeKey={this.props.changeKey}/>
+					<Key keyValue="F#/Gb" keyName="Gb" changeKey={this.props.changeKey}/>
+				</div>
 				<Key keyValue="G" keyName="G" changeKey={this.props.changeKey}/>
-				<Key keyValue="G#/Ab" keyName="Ab" changeKey={this.props.changeKey}/>
+				<div class="sharpFlatContainer">
+					<Key keyValue="G#/Ab" keyName="Ab" changeKey={this.props.changeKey}/>
+					<Key keyValue="G#/Ab" keyName="Ab" changeKey={this.props.changeKey}/>
+				</div>
 				<Key keyValue="A" keyName="A" changeKey={this.props.changeKey}/>
-				<Key keyValue="A#/Bb" keyName="Bb" changeKey={this.props.changeKey}/>
+				<div class="sharpFlatContainer">
+					<Key keyValue="A#/Bb" keyName="Bb" changeKey={this.props.changeKey}/>
+					<Key keyValue="A#/Bb" keyName="Bb" changeKey={this.props.changeKey}/>
+				</div>
 				<Key keyValue="B" keyName="B" changeKey={this.props.changeKey}/>
 			</div>
 		);

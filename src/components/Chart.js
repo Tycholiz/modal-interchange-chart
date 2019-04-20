8714bd0a387505a48fb513103ba5eec182f4435c
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-04-19 18:58:07 -0700

    whole cells and half cells distinquishable

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -6,92 +6,76 @@ class Chart extends Component {
 		const { currentKey, currentNotation } = this.props;
 		return (
 			<div className="chartWrapper">
-				<table>
-					{/* <thead>
-						<tr>
-							<th scope="col" class="cell cell-whole">Mode</th>
-							<th scope="col" class="cell cell-whole">I</th>
-							<th scope="col" class="cell cell-whole">III</th>
-							<th scope="col" class="cell">II</th>
-							<th scope="col" class="cell cell-whole">IV</th>
-							<th scope="col" class="cell cell-whole">V</th>
-							<th scope="col" class="cell cell-whole">VI</th>
-							<th scope="col" class="cell">VII</th>
-						</tr>
-					</thead> */}
-					<tbody>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-whole">Dm</td>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-							<td class="chord cell cell-whole">C</td>
-						</tr>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-							<td class="chord cell cell-whole">C</td>
-							<td class="chord cell cell-whole">Dm</td>
-						</tr>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-whole">C</td>
-							<td class="chord cell cell-whole">Dm</td>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-						</tr>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-whole">C</td>
-							<td class="chord cell cell-whole">Dm</td>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-						</tr>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-whole">C</td>
-							<td class="chord cell cell-whole">Dm</td>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-						</tr>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-whole">C</td>
-							<td class="chord cell cell-whole">Dm</td>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-						</tr>
-						<tr>
-							<th scope="row" class="mode-instance cell cell-whole">Ionian</th>
-							<td class="chord cell cell-whole">C</td>
-							<td class="chord cell cell-whole">Dm</td>
-							<td class="chord cell cell-half">Em</td>
-							<td class="chord cell cell-whole">F</td>
-							<td class="chord cell cell-whole">G</td>
-							<td class="chord cell cell-whole">Am</td>
-							<td class="chord cell cell-half">Bdim</td>
-						</tr>
-					</tbody>
-				</table>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-whole">Am</div>
+						<div className="chord cell cell-whole">C</div>
+						<div className="chord cell cell-half">Bdim</div>
+					</div>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-whole">Am</div>
+						<div className="chord cell cell-half">Bdim</div>
+						<div className="chord cell cell-whole">C</div>
+					</div>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-whole">C</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">Am</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-half">Bdim</div>
+					</div>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-whole">C</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-whole">Am</div>
+						<div className="chord cell cell-half">Bdim</div>
+					</div>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-whole">C</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-half">Bdim</div>
+						<div className="chord cell cell-whole">Am</div>
+					</div>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-whole">C</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-half">Bdim</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-whole">Am</div>
+					</div>
+					<div className="row">
+						<div className="mode-instance cell cell-whole">Ionian</div>
+						<div className="chord cell cell-half">Em</div>
+						<div className="chord cell cell-whole">C</div>
+						<div className="chord cell cell-whole">Dm</div>
+						<div className="chord cell cell-half">Bdim</div>
+						<div className="chord cell cell-whole">F</div>
+						<div className="chord cell cell-whole">G</div>
+						<div className="chord cell cell-whole">Am</div>
+					</div>
 			</div>
 		);
 	}

0f4668365a6d57c9c2d3672ec7b64d5c627d5bd4
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-04-20 17:16:50 -0700

    remove react-ga

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-138661181-1', {
-	standardImplementation: true
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
